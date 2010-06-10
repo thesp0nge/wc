72c91d71fc243b4ab37892c4b25e61b00ee88d07
Paolo Perego <thesp0nge@gmail.com>
2010-06-10 11:35:39 +0200

    Output was done over non sorted values... shame on me. Fixed now. Have fun!

--- lib/wc.rb
+++ lib/wc.rb
@@ -1,35 +1,34 @@
 class Wc 
-  attr :filename
+  attr_reader :filename, :occurrences
   
   def initialize(filename)
     @filename = filename
-    @hash = read
-    p @hash.inspect
-    Array(hash).sort { |one, two| -(one[1] <=> two[1]) }
+    @occurrences = read
+    @sorted = Array(occurrences).sort { |one, two| -(one[1] <=> two[1]) }
     
   end
   
   def to_text
-      @hash.each { |elem|
+      @sorted.each { |elem|
         puts "\"#{elem[0]}\" has #{elem[1]} occurrences"
       }
   end
   
   def to_json
-    puts @hash.to_json
+    @sorted.to_json
   end
   
   private
   
   def read() 
-    hash = Hash.new { |h, k| h[k] = 0 }
+    occurrences = Hash.new { |h, k| h[k] = 0 }
     File.open(@filename, "r") { |f|
           f.each_line { |line|
             words = line.split
-            words.each { |w| hash[w] += 1 }
+            words.each { |w| occurrences[w] += 1 }
           }
         }
-    hash
+    occurrences
   end
   
 end
