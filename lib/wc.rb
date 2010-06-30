0da62c8c415efa3eee7206fd04c2a1b7498171db
Paolo Perego <thesp0nge@gmail.com>
2010-06-30 17:50:21 +0200

    Added '-w' option.

--- lib/wc.rb
+++ lib/wc.rb
@@ -1,17 +1,23 @@
 class Wc 
-  attr_reader :filename, :occurrences
+  attr_reader :filename, :occurrences, :words
   
-  def initialize(filename)
+  def initialize(filename, words)
     @filename = filename
     @occurrences = read
     @sorted = Array(occurrences).sort { |one, two| -(one[1] <=> two[1]) }
-    
+    @words = words
   end
   
   def to_text
+    if @words == -1
       @sorted.each { |elem|
         puts "\"#{elem[0]}\" has #{elem[1]} occurrences"
       }
+    else 
+      @sorted[0..@words-1].each { |elem|
+        puts "\"#{elem[0]}\" has #{elem[1]} occurrences"
+      }
+    end
   end
   
   def to_json
